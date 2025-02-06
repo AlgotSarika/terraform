678c2bf38a3579cff7824fbd0544ca5d40a3ec97
Algot Sarika <sarika@joindevops.com>
2025-02-06 16:25:50 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,23 @@
+variable "instances" {
+    default = ["mysql", "backend", "frontend"]
+  
+}
+
+variable "zone_id" {
+    default = "Z08625042U0IMS33AVUS6"
+  
+}
+
+variable "domain_name" {
+    default = "algot.online"
+  
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        project = "expense"
+        Environment = "dev"
+    }
+  
+}
